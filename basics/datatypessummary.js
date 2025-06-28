ff5c536e5dc931c79df7c42a8ca3ef6262dc748b
shub-droid <shubhammallick1947@gmail.com>
2025-06-28 18:40:22 +0000

    commit

--- basics/datatypessummary.js
+++ basics/datatypessummary.js
@@ -2,8 +2,31 @@
 // 7 Types: String,Number,Boolearn,null,undefined,symbol(for unique value),big int
 
 
+const id=Symbol('123')
+const anotherID=Symbol('123')
 
+console.log(id==anotherID)
 
 //Reference Type : Non primitive
 //Arrays,Objects,functions
+const heros=["shaktiman","batman","doga"]
+let shubham={
+    name:"hitesh",
+    age:"19"
+}
+//null ka typeof ka output object aata hai
+//function ka data type objet function bola jaata hai
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
 
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object  */
