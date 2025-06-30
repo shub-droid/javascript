b6eea068b4dac4eacfbaada6a2f51e710bfe555f
shub-droid <shubhammallick1947@gmail.com>
2025-06-30 07:56:02 +0000

    commit

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+//Array
+
+//const myArr=[1,2,3,'shubham']
+
+//JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+//A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may also cause the other object to change too. That behavior contrasts with the behavior of a deep copy, in which the source and copy are completely independent.
+//const myheroes=['shubham','mallick']
+//const friends=new Array('kishan','rishi','prakash')
+
+//Array Methods
+//console.log("Method of arrays")
+
+//myArr.push(9)
+//console.log(myArr)
+//myArr.unshift(9)
+//console.log(myArr)
+//myArr.shift() //inital waala haat jaata hai
+//console.log(myArr)
+//console.log(myArr.includes(9))
+//onsole.log(myArr.indexOf(9))
+
+//const newArr=myArr.join()
+
+//console.log(myArr)
+//console.log(newArr) //after join operation string is returned not a array
+
+//slice and splice
+console.log("slice and splice")
+// Differnce between Slice and Splice
+/* Splice Operation manupulate the Array.*/
+A1=new Array(1,2,3,4,5,6,7,8,9)
+console.log(A1)
+const A2=A1.slice(1,3)
+console.log(A2)
+console.log(A1)
+const A3=A1.splice(1,3)
+console.log(A3)
+console.log(A1)
