d123f0de1f66144c2bb4977dc90a880d1248c077
shub-droid <shubhammallick1947@gmail.com>
2025-07-03 06:14:10 +0000

    commit

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    //named IIFE
+    console.log('DB CONNECTED')
+})();//;to end the function
+
+(()=>{
+    console.log("hello")
+})()
+
+
+//+++++++++Immediately Invoked Function Expressions IIFE 
+//to remove the polution of global scope we use IIFE
