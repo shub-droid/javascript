ab54aae92b89da754ad535fb41f09c24e66554ed
shub-droid <shubhammallick1947@gmail.com>
2025-07-05 10:00:14 +0000

    commit

---
+++ 05_iterations/one.js
@@ -0,0 +1,12 @@
+// for loop
+
+for (let i= 0;i< 10;i++){
+    console.log("outer loop"+i)
+    for(let j=0;j<i;j++){
+        console.log("This is the inner value "+j);
+    }
+    
+}
+
+//break and continue
+//continue ek baar maaf krdo
