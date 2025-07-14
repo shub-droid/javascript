38dc7528dc3c68e6fee2318eb922de59e02d313e
shub-droid <shubhammallick1947@gmail.com>
2025-07-14 17:12:06 +0000

    commit

--- 09_Advance_1/promise.js
+++ 09_Advance_1/promise.js
@@ -1,4 +1,4 @@
-const promiseOne=new Promise(function(resolve,reject){
+/*const promiseOne=new Promise(function(resolve,reject){
     //Do an Asynch task
     //DB calls,cypto,network
     setTimeout(function(){
@@ -58,3 +58,51 @@ promisefour.then((user)=>{
     console.log("The Promise is either reject or resolve")
 })// yeah finally block hamesa run krta hai
 //console.log(user) yeah krne se error throw kr rha tha
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+*/
+
+/*async function getAllUsers(){
+   try {
+       const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+         console.log("E: ", error);
+    }
+}
+*/
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response)=>{
+    return response.json()
+})
+.then((response)=>{
+    console.log(response)
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
